e091544ac398473502fc7d33573c6ebdbc233aef
jamesricarte <uhenyou@gmail.com>
2025-02-28 16:49:28 +0800

    final connection of client and server from sir

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const postRoutes = require("../backend/config/postRoutes")
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cors({origin: 'http://127.0.0.1:4200'}))
 app.use("/api", postRoutes);
 
--- backend/config/postRoutes.js
+++ backend/config/postRoutes.js
@@ -2,7 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/posts", (req, res) => {
+router.post("/posts", (req, res) => {
+    const post = req.body;
+    console.log(post);
+
+    res.status(201).json({
+        message: "Post added successfully"
+    })
+})
+
+router.use("/posts", (req, res) => {
     const posts = [
         {id: 1, title: "James", content: "James content"},
         {id: 2, title: "Charles", content: "Charles content"},
